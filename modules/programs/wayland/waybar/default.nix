80f2c383defd117d8b7ba17ee01b3b6555b94f41
angerZen <social@angerzen.com>
2023-09-30 11:18:54 -0500

    more waybar fixes

--- modules/programs/wayland/waybar/default.nix
+++ modules/programs/wayland/waybar/default.nix
@@ -11,17 +11,7 @@
 #    })
 #  ];
   home-manager.users.angerzen = {
- #   programs.waybar = {
-   programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
-    mesonFlags = (oa.mesonFlags or  []) ++ [ "-Dexperimental=true" ];
-    patches = (oa.patches or []) ++ [
-      (pkgs.fetchpatch {
-        name = "fix waybar hyprctl";
-        url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
-        sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
-      })
-    ];
- 
+    programs.waybar = {
       enable = true;
       systemd = {
         enable = false;
@@ -397,6 +387,5 @@
 
                 print(json.dumps(data))
               '';
-    });
-  };
+    };
 }
