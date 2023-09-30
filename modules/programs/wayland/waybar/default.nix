2b6c579d355a55a3b76b60ceaa421918e5e51ffd
angerZen <social@angerzen.com>
2023-09-30 11:12:22 -0500

    waybar fixes

--- modules/programs/wayland/waybar/default.nix
+++ modules/programs/wayland/waybar/default.nix
@@ -1,17 +1,27 @@
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      waybar = prev.waybar.overrideAttrs (oldAttrs: {
-        mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
-        postPatch =
-          (oldAttrs.postPatch or "")
-          + ''
-            sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
-      });
-    })
-  ];
+#  nixpkgs.overlays = [
+#    (final: prev: {
+#      waybar = prev.waybar.overrideAttrs (oldAttrs: {
+#        mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+#        postPatch =
+#          (oldAttrs.postPatch or "")
+#          + ''
+#            sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
+#      });
+#    })
+#  ];
   home-manager.users.angerzen = {
-    programs.waybar = {
+ #   programs.waybar = {
+   programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
+    mesonFlags = (oa.mesonFlags or  []) ++ [ "-Dexperimental=true" ];
+    patches = (oa.patches or []) ++ [
+      (pkgs.fetchpatch {
+        name = "fix waybar hyprctl";
+        url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";
+        sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";
+      })
+    ];
+ 
       enable = true;
       systemd = {
         enable = false;
@@ -132,260 +142,261 @@
 
       style = "
 
-@import '../../.cache/wal/colors-waybar.css';
-* {
-  font-family: JetbrainsMono nerd font;
-  font-weight: bold;
-  font-size: 14px;
-  min-height: 0;
-  transition-property: background-color;
-  transition-duration: 0.5s;
-  transition: all linear 0.5s;
-}
-@keyframes blink_red {
-  to {
-    background-color: rgb(242, 143, 173);
-    color: rgb(26, 24, 38);
-  }
-}
-.warning, .critical, .urgent {
-  animation-name: blink_red;
-  animation-duration: 1s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-  animation-direction: alternate;
-}
-window#waybar {
-  background-color: transparent;
-}
-window>box {
-  margin-left: 16px;
-  margin-right: 16px;
-  margin-top: 12px;
-  margin-bottom: 0px;
-  border: 3px solid @color2;
-  border-radius: 4px;
-  background-color: @background;
-}
-#workspaces {
-  padding: 0px;
-}
-#workspaces button {
-  padding: 4px 6px;
-  margin: 4px 2px;
-  border-radius: 50%;
-  background-color: @background;
-  color: @foreground;
-}
-#workspaces button.active {
-  background-color: @color2;
-  border-radius: 2px;
-  color: @foreground;
-}
-#workspaces button:hover {
-  color: @color2;
-  background: @background;
-  border: 0px solid @color2;
-  box-shadow: inherit;
-  text-shadow: inherit;
-}
-#workspaces button.active:hover {
-  background-color: @color2;
-  color: @foreground;
-}
-tooltip {
-  background-color: @background;
-  border-radius: 0px;
-  border: 2px solid @color2;
-}
-tooltip label {
-  color: @foreground;
-}
-#custom-launcher {
-  font-size: 16px;
-  padding-left: 10px;
-  padding-right: 6px;
-  color: @color2;
-}
-#clock,
-#memory,
-#temperature,
-#custom-weather,
-#cpu,
-#mpd,
-#custom-wall,
-#temperature,
-#backlight,
-#pulseaudio,
-#network,
-#battery,
-#disk,
-#idle_inhibitor
-{
-  padding-left: 8px;
-  padding-right: 8px;
-  color: @foreground;
-}
-
-#cava {
-  margin-left: 10px;
-  color: @color2;
-}
-
-#memory, #cpu, #clock, #pulseaudio, #network {
-  background-color: @color2;
-  border-radius: 2px;
-  padding: 0px 8px;
-  margin: 4px;
-  color: @background;
-}
-
-#tray {
-  margin: 0px 10px 0px 4px;
-}
-
-#memory {
-  border-radius: 2px 0px 0px 2px;
-  margin-right: 0px;
-}
-
-#cpu {
-  border-radius: 0px 2px 2px 0px;
-  margin-left: 0px;
-}
-
-#pulseaudio {
-  border-radius: 2px 0px 0px 2px;
-  margin-right: 0px;
-}
-
-#pulseaudio.microphone {
-  border-radius: 0px 2px 2px 0px;
-  margin-left: 0px;
-  margin-right: 4px;
-}
-
-
-#custom-cava-internal{
-  padding-left: 10px;
-  padding-right: 10px;
-}
-  ";
-    };
-    xdg.configFile."waybar/weather.py".text = ''
-      #!/usr/bin/env python
-      import json
-      import requests
-      from datetime import datetime
-
-      WEATHER_CODES = {
-          '113': '☀️',
-          '116': '⛅️',
-          '119': '☁️',
-          '122': '☁️',
-          '143': '🌫',
-          '176': '🌦',
-          '179': '🌧',
-          '182': '🌧',
-          '185': '🌧',
-          '200': '⛈',
-          '227': '🌨',
-          '230': '❄️',
-          '248': '🌫',
-          '260': '🌫',
-          '263': '🌦',
-          '266': '🌦',
-          '281': '🌧',
-          '284': '🌧',
-          '293': '🌦',
-          '296': '🌦',
-          '299': '🌧',
-          '302': '🌧',
-          '305': '🌧',
-          '308': '🌧',
-          '311': '🌧',
-          '314': '🌧',
-          '317': '🌧',
-          '320': '🌨',
-          '323': '🌨',
-          '326': '🌨',
-          '329': '❄️',
-          '332': '❄️',
-          '335': '❄️',
-          '338': '❄️',
-          '350': '🌧',
-          '353': '🌦',
-          '356': '🌧',
-          '359': '🌧',
-          '362': '🌧',
-          '365': '🌧',
-          '368': '🌨',
-          '371': '❄️',
-          '374': '🌧',
-          '377': '🌧',
-          '386': '⛈',
-          '389': '🌩',
-          '392': '⛈',
-          '395': '❄️'
-      }
-
-      data = {}
-
-
-      weather = requests.get("https://wttr.in/mymensingh?format=j1").json()
-
-
-      def format_time(time):
-          return time.replace("00", "").zfill(2)
-
-
-      def format_temp(temp):
-          return (hour['FeelsLikeF']+"°").ljust(3)
-
-
-      def format_chances(hour):
-          chances = {
-              "chanceoffog": "Fog",
-              "chanceoffrost": "Frost",
-              "chanceofovercast": "Overcast",
-              "chanceofrain": "Rain",
-              "chanceofsnow": "Snow",
-              "chanceofsunshine": "Sunshine",
-              "chanceofthunder": "Thunder",
-              "chanceofwindy": "Wind"
+          @import '../../.cache/wal/colors-waybar.css';
+          * {
+            font-family: JetbrainsMono nerd font;
+            font-weight: bold;
+            font-size: 14px;
+            min-height: 0;
+            transition-property: background-color;
+            transition-duration: 0.5s;
+            transition: all linear 0.5s;
+          }
+          @keyframes blink_red {
+            to {
+              background-color: rgb(242, 143, 173);
+              color: rgb(26, 24, 38);
+            }
+          }
+          .warning, .critical, .urgent {
+            animation-name: blink_red;
+            animation-duration: 1s;
+            animation-timing-function: linear;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+          }
+          window#waybar {
+            background-color: transparent;
+          }
+          window>box {
+            margin-left: 16px;
+            margin-right: 16px;
+            margin-top: 12px;
+            margin-bottom: 0px;
+            border: 3px solid @color2;
+            border-radius: 4px;
+            background-color: @background;
+          }
+          #workspaces {
+            padding: 0px;
+          }
+          #workspaces button {
+            padding: 4px 6px;
+            margin: 4px 2px;
+            border-radius: 50%;
+            background-color: @background;
+            color: @foreground;
+          }
+          #workspaces button.active {
+            background-color: @color2;
+            border-radius: 2px;
+            color: @foreground;
+          }
+          #workspaces button:hover {
+            color: @color2;
+            background: @background;
+            border: 0px solid @color2;
+            box-shadow: inherit;
+            text-shadow: inherit;
+          }
+          #workspaces button.active:hover {
+            background-color: @color2;
+            color: @foreground;
+          }
+          tooltip {
+            background-color: @background;
+            border-radius: 0px;
+            border: 2px solid @color2;
+          }
+          tooltip label {
+            color: @foreground;
+          }
+          #custom-launcher {
+            font-size: 16px;
+            padding-left: 10px;
+            padding-right: 6px;
+            color: @color2;
+          }
+          #clock,
+          #memory,
+          #temperature,
+          #custom-weather,
+          #cpu,
+          #mpd,
+          #custom-wall,
+          #temperature,
+          #backlight,
+          #pulseaudio,
+          #network,
+          #battery,
+          #disk,
+          #idle_inhibitor
+          {
+            padding-left: 8px;
+            padding-right: 8px;
+            color: @foreground;
           }
 
-          conditions = []
-          for event in chances.keys():
-              if int(hour[event]) > 0:
-                  conditions.append(chances[event]+" "+hour[event]+"%")
-          return ", ".join(conditions)
+          #cava {
+            margin-left: 10px;
+            color: @color2;
+          }
+
+          #memory, #cpu, #clock, #pulseaudio, #network {
+            background-color: @color2;
+            border-radius: 2px;
+            padding: 0px 8px;
+            margin: 4px;
+            color: @background;
+          }
+
+          #tray {
+            margin: 0px 10px 0px 4px;
+          }
+
+          #memory {
+            border-radius: 2px 0px 0px 2px;
+            margin-right: 0px;
+          }
+
+          #cpu {
+            border-radius: 0px 2px 2px 0px;
+            margin-left: 0px;
+          }
+
+          #pulseaudio {
+            border-radius: 2px 0px 0px 2px;
+            margin-right: 0px;
+          }
+
+          #pulseaudio.microphone {
+            border-radius: 0px 2px 2px 0px;
+            margin-left: 0px;
+            margin-right: 4px;
+          }
 
 
-      data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
-          " "+weather['current_condition'][0]['temp_F']+"°"
+          #custom-cava-internal{
+            padding-left: 10px;
+            padding-right: 10px;
+          }
+            ";
+              };
+              xdg.configFile."waybar/weather.py".text = ''
+                #!/usr/bin/env python
+                import json
+                import requests
+                from datetime import datetime
 
-      data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
-      data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeF']}°\n"
-      data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedMiles']}Mph\n"
-      data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
-      for i, day in enumerate(weather['weather']):
-          data['tooltip'] += f"\n<b>"
-          if i == 0:
-              data['tooltip'] += "Today, "
-          if i == 1:
-              data['tooltip'] += "Tomorrow, "
-          data['tooltip'] += f"{day['date']}</b>\n"
-          data['tooltip'] += f"⬆️ {day['maxtempF']}° ⬇️ {day['mintempF']}° "
-          data['tooltip'] += f"🌅 {day['astronomy'][0]['sunrise']} 🌇 {day['astronomy'][0]['sunset']}\n"
-          for hour in day['hourly']:
-              if i == 0:
-                  if int(format_time(hour['time'])) < datetime.now().hour-2:
-                      continue
-              data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
+                WEATHER_CODES = {
+                    '113': '☀️',
+                    '116': '⛅️',
+                    '119': '☁️',
+                    '122': '☁️',
+                    '143': '🌫',
+                    '176': '🌦',
+                    '179': '🌧',
+                    '182': '🌧',
+                    '185': '🌧',
+                    '200': '⛈',
+                    '227': '🌨',
+                    '230': '❄️',
+                    '248': '🌫',
+                    '260': '🌫',
+                    '263': '🌦',
+                    '266': '🌦',
+                    '281': '🌧',
+                    '284': '🌧',
+                    '293': '🌦',
+                    '296': '🌦',
+                    '299': '🌧',
+                    '302': '🌧',
+                    '305': '🌧',
+                    '308': '🌧',
+                    '311': '🌧',
+                    '314': '🌧',
+                    '317': '🌧',
+                    '320': '🌨',
+                    '323': '🌨',
+                    '326': '🌨',
+                    '329': '❄️',
+                    '332': '❄️',
+                    '335': '❄️',
+                    '338': '❄️',
+                    '350': '🌧',
+                    '353': '🌦',
+                    '356': '🌧',
+                    '359': '🌧',
+                    '362': '🌧',
+                    '365': '🌧',
+                    '368': '🌨',
+                    '371': '❄️',
+                    '374': '🌧',
+                    '377': '🌧',
+                    '386': '⛈',
+                    '389': '🌩',
+                    '392': '⛈',
+                    '395': '❄️'
+                }
+
+                data = {}
 
 
-      print(json.dumps(data))
-    '';
-  };
+                weather = requests.get("https://wttr.in/mymensingh?format=j1").json()
+
+
+                def format_time(time):
+                    return time.replace("00", "").zfill(2)
+
+
+                def format_temp(temp):
+                    return (hour['FeelsLikeF']+"°").ljust(3)
+
+
+                def format_chances(hour):
+                    chances = {
+                        "chanceoffog": "Fog",
+                        "chanceoffrost": "Frost",
+                        "chanceofovercast": "Overcast",
+                        "chanceofrain": "Rain",
+                        "chanceofsnow": "Snow",
+                        "chanceofsunshine": "Sunshine",
+                        "chanceofthunder": "Thunder",
+                        "chanceofwindy": "Wind"
+                    }
+
+                    conditions = []
+                    for event in chances.keys():
+                        if int(hour[event]) > 0:
+                            conditions.append(chances[event]+" "+hour[event]+"%")
+                    return ", ".join(conditions)
+
+
+                data['text'] = WEATHER_CODES[weather['current_condition'][0]['weatherCode']] + \
+                    " "+weather['current_condition'][0]['temp_F']+"°"
+
+                data['tooltip'] = f"<b>{weather['current_condition'][0]['weatherDesc'][0]['value']} {weather['current_condition'][0]['temp_C']}°</b>\n"
+                data['tooltip'] += f"Feels like: {weather['current_condition'][0]['FeelsLikeF']}°\n"
+                data['tooltip'] += f"Wind: {weather['current_condition'][0]['windspeedMiles']}Mph\n"
+                data['tooltip'] += f"Humidity: {weather['current_condition'][0]['humidity']}%\n"
+                for i, day in enumerate(weather['weather']):
+                    data['tooltip'] += f"\n<b>"
+                    if i == 0:
+                        data['tooltip'] += "Today, "
+                    if i == 1:
+                        data['tooltip'] += "Tomorrow, "
+                    data['tooltip'] += f"{day['date']}</b>\n"
+                    data['tooltip'] += f"⬆️ {day['maxtempF']}° ⬇️ {day['mintempF']}° "
+                    data['tooltip'] += f"🌅 {day['astronomy'][0]['sunrise']} 🌇 {day['astronomy'][0]['sunset']}\n"
+                    for hour in day['hourly']:
+                        if i == 0:
+                            if int(format_time(hour['time'])) < datetime.now().hour-2:
+                                continue
+                        data['tooltip'] += f"{format_time(hour['time'])} {WEATHER_CODES[hour['weatherCode']]} {format_temp(hour['FeelsLikeC'])} {hour['weatherDesc'][0]['value']}, {format_chances(hour)}\n"
+
+
+                print(json.dumps(data))
+              '';
+    };
+  });
 }
