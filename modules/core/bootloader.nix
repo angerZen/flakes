8a7f792c8e95b1129957281a6808ea9fe2ad38dc
angerZen <social@angerzen.com>
2023-10-21 11:20:45 -0500

    added fixes for Star Citizen and added stream deck support

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,4 +2,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };
 }
