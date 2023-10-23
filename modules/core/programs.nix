4ef5681887ff9c073bf987b5aa708889d7e78cdf
angerZen <social@angerzen.com>
2023-10-23 17:22:00 -0500

    update for 1password gui black screen

--- modules/core/programs.nix
+++ modules/core/programs.nix
@@ -1,4 +1,24 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  outputs,
+  lib,
+  config,
+  ...
+}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      _1password-gui = prev._1password-gui.overrideAttrs (oldAttrs: {
+        postInstall =
+          (oldAttrs.postInstall or "")
+          + ''
+            substituteInPlace $out/share/applications/1password.desktop \
+            --replace "Exec=1password" "Exec=1password --disable-gpu-sandbox"
+          '';
+      });
+    })
+  ];
+
   programs = {
     dconf.enable = true;
     steam.enable = true;
