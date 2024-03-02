f59e318286c6ac9dc3ce75207bbe10a8d5d2f719
angerZen <social@angerzen.com>
2024-03-02 08:13:20 -0600

    Pipewire change and flake updates

--- modules/core/pipewire.nix
+++ modules/core/pipewire.nix
@@ -1,11 +1,10 @@
 {
   sound.enable = false;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    # alsa.enable = true;
+    # alsa.support32Bit = true;
     pulse.enable = true;
   };
 }
